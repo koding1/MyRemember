3c00ef74ccf9e267ece9359dcb94e5fdedc00bd9
koding1 <koding1@naver.com>
2022-01-03 18:09:52 +0900

    3.3

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const idea = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const [text, setText] = useState("");
+  const onChangeText = (payload) => setText(payload);
+
+  const [toDos, setToDos] = useState({});
+
+  const addToDo = () => {
+    if (text === "") {
+      return
+    }
+    // save to do
+    const tmp = {[Date.now()] : {text:text, WorkOn:working}}; // https://koonsland.tistory.com/146
+    const newToDo = Object.assign({}, toDos,tmp);
+
+    setText("");
+    console.log(newToDo);
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-
-        <TouchableOpacity>s
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Idea</Text>
+        <TouchableOpacity onPress={idea}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+          >
+            Idea
+          </Text>
         </TouchableOpacity>
-        
+      </View>
+
+      <View>
+        <TextInput
+          value={text}
+          returnKeyType="done"
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          placeholder={working ? "할 일 :" : "아이디어 :"}
+          style={styles.input}
+        />
       </View>
     </View>
   );
@@ -25,11 +70,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
-    paddingHorizontal: 20,
+    paddingHorizontal: 20, // padding left and right
   },
   header: {
     justifyContent: "space-between",
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 50,
   },
   btnText: {
@@ -37,5 +82,12 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "600",
     fontSize: 45,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 16, // padding top and bottom
+    fontSize: 18,
+    borderRadius: 20,
+    marginTop: 20,
   },
 });
