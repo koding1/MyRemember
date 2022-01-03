10b5ac654ff7a0f0929e806e604fd265fa025117
koding1 <koding1@naver.com>
2022-01-03 15:42:15 +0900

    3.1

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+
+        <TouchableOpacity>s
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Idea</Text>
+        </TouchableOpacity>
+        
+      </View>
     </View>
   );
 }
@@ -13,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  btnText: {
+    // color: theme.grey,
+    color: "white",
+    fontWeight: "600",
+    fontSize: 45,
   },
 });
